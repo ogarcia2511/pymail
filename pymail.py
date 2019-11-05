e3da9b570011b2c40dc54135b326efa7730c62c0
ogarcia2511 <ogscu1179@gmail.com>
2019-11-04 21:42:46 -0800

    Addition of POP3

--- pymail.py
+++ pymail.py
@@ -3,33 +3,43 @@
 
 import smtplib, poplib, ssl
 import subprocess
+import getpass
 from os import system
+
+
+def send(email_address):
+    receiver_email = input("Please enter receiver's email address: ")
+    smtp_server.sendmail(email_address, receiver_email, "SUBJECT: Test\nHi from me!\n")
+
+def read():
+    pass
 
 _ = system.("clear")
 
 print("- * - * - * P y m a i l * - * - * -")
 print("Welcome to the Python Gmail client!")
 
+smtp_port = 465 # with SSL
+pop3_port = 995 # with SSL
 
-
-port = 465  # For SSL
 email_address = input("Please enter your email address: ") # converted raw_input to input
-password = input("Please enter your password: ", )
-receiver_email = input("Please enter receiver's email address: ")
+password = getpass.getpass("Please enter your password: ")
 
 
-print '\nNow contacting the mail server...'
+print('\nInitializing mail server...')
 # STUDENT WORK
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', smtp_port)
+pop3_server = poplib.POP3_SSL('pop.gmail.com', pop3_port)
+smtp_server.ehlo()
 
-server.ehlo()
+smtp_server.login(email_address, password)
+print("smtp login successful!")
+pop3_server.user(email_address)
+pop3_server.pass_(password)
+print("pop3 login successful!")
 
-server.login(email_address, password)
+status = pop3_server.stat()
+print(status)
 
-print '\nSending email...'
-
-
-# STUDENT WORK
-server.sendmail(email_address, receiver_email, "SUBJECT: Test\nHi from me!\n%s" % ping_response)
-
-server.quit()
+smtp_server.quit()
+pop3_server.quit()
